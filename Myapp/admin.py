e9beb48718d879aef8d946b516622bdb04f6cb53
gabrielking1 <gkgabrielking@gmail.com>
2023-05-18 16:54:49 +0100

    updated

--- Myapp/admin.py
+++ Myapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Feedback
 
 # Register your models here.
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('fullname',
+    'email',
+    'phone',
+    'country',
+    'feedback',
+    'date')
+    readonly_fields = (
+        'fullname',
+    'email',
+    'phone',
+    'country',
+    'feedback',
+    'date'
+    )
